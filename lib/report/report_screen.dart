801aced4c6ac7437df2c909bd759c626d3b08821
Aman Kumar <thisisamank@gmail.com>
2021-10-27 10:25:55 +0530

    report screen added

--- lib/report/report_screen.dart
+++ lib/report/report_screen.dart
@@ -5,6 +5,7 @@ import 'package:mirror/constants/colors.dart';
 import 'package:mirror/home/home_screen.dart';
 import 'package:mirror/models/report.dart';
 import 'package:mirror/shared/widgets/mirror.dart';
+import 'package:mirror/splash/widgets/dialog.dart';
 
 class ReportScreen extends StatelessWidget {
   ReportScreen({Key? key}) : super(key: key);
@@ -15,7 +16,8 @@ class ReportScreen extends StatelessWidget {
 
   Future<Results> fetchData() async {
     final json = await rootBundle.loadString('assets/output.json');
-    return Results.fromRawJson(json);
+    return Future.delayed(
+        Duration(seconds: 10), () => Results.fromRawJson(json));
   }
 
   _getEmojiCard(List<String> emojis, int selected, String title) {
@@ -38,15 +40,18 @@ class ReportScreen extends StatelessWidget {
                 final isSelected = i == selected;
                 i++;
                 return Container(
-                  width: 42,
+                  width: 50,
+                  height: 50,
                   decoration: BoxDecoration(
                     color: isSelected ? MColor.skyBlue : Colors.transparent,
                     borderRadius: BorderRadius.circular(100),
                   ),
-                  child: Text(
-                    emoji,
-                    style: TextStyle(
-                      fontSize: isSelected ? 32 : 24,
+                  child: Center(
+                    child: Text(
+                      emoji,
+                      style: TextStyle(
+                        fontSize: isSelected ? 32 : 24,
+                      ),
                     ),
                   ),
                 );
@@ -58,12 +63,72 @@ class ReportScreen extends StatelessWidget {
     );
   }
 
-  Widget _getResultView(Results result) {
+  _getCard(List<String> words, String title) {
     return Container(
-      child: _getEmojiCard(
-        _facialEmoji,
-        result.expression - 1,
-        'Facial Expressions',
+      padding: EdgeInsets.all(16),
+      width: 300,
+      decoration: BoxDecoration(
+        color: MColor.background,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Column(
+        children: [
+          MirrorText(text: title, size: 20),
+          Wrap(
+            spacing: 8,
+            runSpacing: 4,
+            children: [
+              ...words.map((e) => MirrorPill(text: e, isSelected: true))
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _getResultView(Results result) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            child: _getEmojiCard(
+              _facialEmoji,
+              result.expression - 1,
+              'Facial Expressions',
+            ),
+          ),
+          SizedBox(height: 16),
+          Container(
+            child: _getEmojiCard(
+              _gestureEmoji,
+              result.geatures - 1,
+              'Gestures',
+            ),
+          ),
+          SizedBox(height: 16),
+          Container(
+            child: _getEmojiCard(
+              _loudnessEmoji,
+              result.loudness - 1,
+              'Loudness',
+            ),
+          ),
+          SizedBox(height: 16),
+          Container(
+            child: _getCard(
+              result.misspelledWords,
+              'Misspelled Words',
+            ),
+          ),
+          SizedBox(height: 16),
+          Container(
+            child: _getCard(
+              result.frequentlyUsed,
+              'Frequently used Words',
+            ),
+          ),
+          SizedBox(height: 16),
+        ],
       ),
     );
   }
@@ -72,22 +137,30 @@ class ReportScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            MirrorAppBar(title: 'Report'),
-            FutureBuilder<Results>(
-              future: fetchData(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.done) {
-                  return _getResultView(snapshot.data!);
-                } else {
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-              },
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              MirrorAppBar(title: 'Report'),
+              FutureBuilder<Results>(
+                future: fetchData(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    return _getResultView(snapshot.data!);
+                  } else {
+                    return Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                        MirrorText(text: "Video uploading and processing...")
+                      ],
+                    );
+                  }
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
